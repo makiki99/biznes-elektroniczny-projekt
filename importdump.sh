24411972040cd0cb21d13d958c437439a238759f
makiki <mat.gawro@gmail.com>
2021-01-22 00:42:56 +0100

    magia sqlowania

--- importdump.sh
+++ importdump.sh
@@ -1,2 +1,3 @@
-echo "CREATE DATABASE IF NOT EXISTS db5;" | mysql --host="biznes-db" --port=3306 --user "user_5" --password "ach ten projekt"
-mysql --host="biznes-db" --port=3306 --user "user_5" --password "ach ten projekt" 'db_5' < /var/www/html/dump.sql
+mysql --host="biznes-db" --port=3306 --user "root" -pdev < /var/www/html/user.sql
+mysql --host="biznes-db" --port=3306 --user "user_5" -p"ach ten projekt" 'db_5' < /var/www/html/create.sql
+mysql --host="biznes-db" --port=3306 --user "user_5" -p"ach ten projekt" 'db_5' < /var/www/html/dump.sql
---
+++ user.sql
@@ -0,0 +1,2 @@
+CREATE USER 'user_5'@'localhost' IDENTIFIED BY 'ach ten projekt';
+CREATE USER 'user_5'@'%' IDENTIFIED BY 'ach ten projekt';
